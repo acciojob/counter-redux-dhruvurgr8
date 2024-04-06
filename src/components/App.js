1fe427d6ba84d41be95afa1928ebf85abf259795
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-04-06 05:51:57 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+const App = () =>  {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <h1>Practice</h1>
+      <h2>Count:{count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </>
+  );
 }
 
 export default App
